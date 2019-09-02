ae095613f53306fa01d66a6f4f9dc15654e06f01
michalprzybylik@gmail.com <michalprzybylik@gmail.com>
2019-09-02 19:05:53 +0200

    1.23

--- 1.23.c
+++ 1.23.c
@@ -2,33 +2,49 @@
 
 int main()
 {
-    int c, d;
+    int c, d, tmp;
     int inqma, inqmb;
     inqma = inqmb = 0;
     while ((c = getchar()) != EOF)
     {
         if (c == '\'')
+        {
             if (inqma == 0)
                 inqma = 1;
             else
                 inqma = 0;
-
+        }
         if (c == '"')
+        {
             if (inqmb == 0)
                 inqmb = 1;
             else
                 inqmb = 0;
-
+        }
         if (c == '/' && inqma == 0 && inqmb == 0)
         {
             d = getchar();
             if (d == '/')
-                while ((d = getchar()) != '\n')
+            {
+                while ((getchar()) != '\n')
                     ;
+                putchar('\n');
+            }
             else if (d == '*')
-                while ((d = getchar()) != '*')
-                    if(getchar() == '/')
-                        break;
+                {
+                    c = 0;
+                while ((d = getchar()) != '/' || c != 1)
+                    {
+                    c = 0;
+                    if(d == '*')
+                        c = 1;
+                    }
+                }
+            else
+            {
+                putchar(c);
+                putchar(d);
+            }
         }
         else
             putchar(c);
