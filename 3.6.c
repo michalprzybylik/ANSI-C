4bc0fb5c95bf0758e5829e726510d0899cf9e3ca
Michał Przybylik <michalprzybylik@MacBook-Air-michalprzybylik.local>
2019-10-28 02:23:19 +0100

    Exercises 3.[4-6]

---
+++ 3.6.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#define STRLEN 1024
+
+int abs(int n);
+void reverse(char s[]);
+void itoa(int n, char s[], int m);
+
+int main()
+{
+    char str[STRLEN];
+    itoa(-12234, str, 5);
+    printf("%s\n", str);
+    return 0;
+}
+
+int abs(int n)
+{
+    return n >= 0 ? n : -n;
+}
+
+void reverse(char s[])
+{
+    int f, b, tmp;
+    f = b = 0;
+    while (s[++b])
+        ;
+    --b;
+    for (; f < b; ++f, --b)
+        tmp = s[f], s[f] = s[b], s[b] = tmp;
+}
+
+void itoa(int n, char s[], int m)
+{
+    int i, sign;
+    sign = (n < 0);
+    i = 0;
+    do
+    {
+        s[i++] = abs(n % 10) + '0';
+    } while (n /= 10);
+    if (sign)
+        s[i++] = '-';
+    while(i < m)
+        s[i++] = ' ';
+    s[i] = '\0';
+    reverse(s);
+}
