4c60ac775d4f5e7ec1ee1e05c07771d0f7814bb8
Michał Przybylik <michalprzybylik@MacBook-Air-michalprzybylik.local>
2020-03-02 19:21:32 +0100

    1.05 comment

--- 1.05.c
+++ 1.05.c
@@ -1,11 +1,15 @@
-#include<stdio.h>
+/*
+Ćwiczenie 1.5. Zmień program przekształcania temperatur tak, aby wypisywał zestawienie
+w odwrotnej kolejności, to znaczy od 300 stopni do zera.
+*/
+#include <stdio.h>
 #define LOWER 0
 #define STEP 20
 #define UPPER 300
 
 int main()
 {
-    for(int fahr = UPPER; fahr >=LOWER; fahr = fahr - STEP)
-        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    for (int fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
+        printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
     return 0;
 }
