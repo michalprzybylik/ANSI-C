40c95a05625abe660de997603bd425d43d00ecac
Michał Przybylik <michalprzybylik@pc4.home>
2020-04-08 08:19:15 +0200

    mosix

---
+++ lab/mosix.c
@@ -0,0 +1,97 @@
+/*Michal Przybylik TI2
+ 
+Program losuje ilosc liczb naturalnych wiekszych od 1 podanych w stalej symbolicznej NUMSIZE.
+Nastepnie oblicza sume ich najwiekszych dzielnikow calkowitych.
+ 
+1 parametr wywolania - n liczba tworzonych prcesow potomnych
+ 
+Proces rodzica tworzy dwa potoki oraz n potomkow (n jest parametrem wywolania programu).
+Nastepnie losuje i wysyla wszystkie liczby do potoku. Procesy potomne odbieraja, przetwarzaja
+i odsylaja wyniki do rodzica, dopoki nie odbiora z potoku liczby -1. Kiedy tak sie stanie koncza
+prace. Proces rodzica odbiera liczby od potomkow przez potok i sumuje je. Gdy wszystkie liczby 
+zostana odebrane, rodzic wysyla do potoku liczbe -1, co jest sygnalem dla potomkow do zakonczenia
+pracy
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+
+#define NUMSIZE 10  //liczba liczb do przetworzenia
+#define NUMRANGE 16 //zakres liczby od 2 do NUMRANGE
+#define ENDDATA -1
+
+int maxdiv(int n); //deklaracja funkcji zwracajacej najwiekszy calkowity dzielnik liczby naturalnej n wiekszej od 1
+
+int main(int argc, char *argv[])
+{
+        if (argc != 2)
+                perror("skladnia \"nazwa_programu liczba_potomkow\""), exit(1);
+
+        int n = atoi(argv[1]);
+
+        int sendpipe[2], recpipe[2];         //utworzenie dwoch potokow, potok sendpipe odpowiada za dane przesylane
+        if (pipe(sendpipe) || pipe(recpipe)) //od rodzica do potomkow, potok recpipe za dane wynikowe od potomkow do rodzica
+                perror("blad tworzenia potoku"); 
+                exit(-2);
+        int tosend, rec;
+
+        long int sum = 0;
+
+        int pid;
+        int i = n;                   //utworzenie n procesow potomnych przez rodzica
+        while (i--)                  //nowo utworzony proces potomny od razu wychodzi
+                if (!(pid = fork())) //z petli (jego pid jest rowny 0)
+                        break;
+
+        switch (pid) //sprawdz czy proces potomka czy rodzica
+        {
+        case -1:
+                perror("blad tworzenia procesu rodzica");
+                exit(-1);
+                break;
+        case 0:                                                                //proces potomka
+                while (read(sendpipe[0], &rec, sizeof(rec)) && rec != ENDDATA) //odbieraj, przetwarzaj i odsylaj dane dopoki nie otrzymasz z potoku liczby -1
+                {
+                        tosend = maxdiv(rec);                       //wykonaj obliczenia
+                        write(recpipe[1], &tosend, sizeof(tosend)); //odeslij wynik do rodzica
+                }
+                exit(0);
+                break;
+        default:                        //proces rodzica
+                i = NUMSIZE;            //wylosuj NUMSIZE liczb,
+                printf("Wylosowano: "); //wypisz je na wyjscie
+                while (i--)             //i wyslij do potomkow przez potok
+                {
+                        printf("%d ", tosend = (rand() % (NUMRANGE - 2)) + 2); //wylosuj liczbe i wypisz ja
+                        write(sendpipe[1], &tosend, sizeof(tosend));           //wyslij wylosowana liczbe do potomka
+                }
+
+                i = NUMSIZE; //odbierz wyniki
+                while (i--)  //i oblicz sume
+                {
+                        read(recpipe[0], &rec, sizeof(rec)); //odbierz wynik z potoku
+                        sum += rec;                          //dodaj go obliczanej sumy
+                }
+                tosend = ENDDATA;                            //gdy wszystkie liczby zostaly przetworzone wyslij liczbe                                            //do wszystkich potomkow, co bedzie
+                write(sendpipe[1], &tosend, sizeof(tosend)); //ENDDATA co bedzie sygnalem do zakonczenia pracy potomkow
+
+                printf("\nSuma najwiekszych dzielnikow tych liczb wynosi: %ld\n", sum); //wypisz wynik
+                return 0;
+                break;
+        }
+}
+
+int maxdiv(int n) //funkcja najwiekszy calkowity dzielnik liczby wiekszej od 1
+{
+        if (!(n % 2))         //jesli liczba jest parzysta, to
+                return n / 2; //jej najwiekszy calkowity dzielnik jest rowny jej polowie
+        else
+        {                                          //gdy liczba jest nieparzysta szukaj najwiekszego dzielnika
+                int i;                             //sprawdzajac reszty z dzielenia tej liczby przez liczby i z
+                for (i = n - 1; i >= sqrt(n); --i) //zakresu [floor(sqrt(n)), n - 1] od konca przedzialu
+                        if (!(n % i))              //jesli i jest pierwsza liczba liczac od konca przedzialu,
+                                return i;          //ktora dzieli n bez reszty to jest ona szukana liczba
+                return 1;                          //w przciwnym wypadku szukana liczba to 1
+        }
+}
---
+++ lab/test.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+int i;
+int main()
+{
+	void inc (void);
+	int z = 1;
+	inc();
+	char z1 = 127;
+	i = 0;
+	short int z2 = 32767;
+	printf("%d\n", z1 + z2);
+	int fun (char);
+	char a;
+	char b;
+	char *pa = &a;
+	fun(1);
+	char *pat = &b;
+	int c = pat - pa; 
+	fun(4);
+	fun(2);
+	printf("%p\n", pa);
+	printf("%p\n", pat);
+	printf("%d\n", pa > pat ? 1 : 0);
+	int num;
+	scanf("%d", &num);
+	int tab[num];
+	for(int i = 0; i < num; i++)
+	{
+		scanf("%d", tab + i);
+	}
+	for(int i = 0; i < num; i++)
+		printf ("%d ", *(tab + i) * 2);
+	int la = 0, lb = 4;
+	printf("\n%d\n", la / lb);
+	printf("\n%d\n", la % lb);
+	int i = 0;
+	printf("%d\n", i = 1 && 0);
+	printf("%d\n", i);
+	i = 12;
+	i = a = 12;
+	i = a++;
+	printf("%d\n", i);
+	printf("z=%d\n", z);
+	inc();
+	printf("z=%d\n", z);
+	return 0;
+	
+}
+
+int fun(char a)
+{
+	char b;
+	char c;
+	c = b;
+	b = c;
+	printf("%p\n", &b);
+	return a; 	
+}
+
+void inc(void)
+{
+	extern int z;
+	z++;
+}
